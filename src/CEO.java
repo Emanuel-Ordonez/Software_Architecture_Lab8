baa1193f58f72eaa4110ed30898600e7c96dcc58
Regulator1904 <ordonezemanuel51@gmail.com>
2020-04-14 13:03:47 -0700

    Need CEO,DirectAdmin,Manager, ProjectLeader, Supervisor

--- src/CEO.java
+++ src/CEO.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class CEO extends Administrator{
 
     public void seeDanger(HReporter_IF r, Hazard h){
-        List<Decision> decisions = new List<Decision>;
+        List<Decision> decisions = new ArrayList<Decision>();
     }
 
     public void implementDecision(List<Decision> ds){
